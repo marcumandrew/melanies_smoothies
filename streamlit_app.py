d3c925ddec809bcb42a99e384b9551729626b419
Andrew Marcum <amarcuma@yahoo.com>
2025-11-30 22:01:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")
+#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 #st.text(smoothiefroot_response.json())
 
